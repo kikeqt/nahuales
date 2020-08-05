45ff8826b8305507820d1fde63f176de38e28d99
Enrique Quijano <kike.qt@gmail.com>
2020-08-05 14:18:20 -0500

    Update active_bits.py
    Incorporation of unittest

--- tools/active_bits.py
+++ tools/active_bits.py
@@ -1,13 +1,12 @@
 __version__ = "$Version: 2.0.0"
 
-import unittest
+from unittest import TestCase
 
 from .active_bits_iterable import Active_bits_iterable
 
 
-
 class Active_bits(Active_bits_iterable):
-    
+
     def active_bits(self, bytes_parameter: bytes):
         """Vector with the positions of active bits in notation little endian
 
@@ -19,7 +18,8 @@ class Active_bits(Active_bits_iterable):
             block = self.bytes_2_integer(bytes_parameter)
 
         else:
-            print('Fatal error (active_bits): The argument must be a byte type not {}'.format(type(argBytes)))
+            print('Fatal error (active_bits): The argument must be a byte type not {}'.format(
+                type(argBytes)))
             exit()
 
         if block == 0:
@@ -32,12 +32,15 @@ class Active_bits(Active_bits_iterable):
                 vActive_bits.append(aBits)
 
             return vActive_bits
-        
 
-class Active_bits_Test(unittest.TestCase):
+
+class Active_bits_Test(TestCase):
     def setUp(self):
-        self.active_bits = Active_bits()
-        
+        self.test_object = Active_bits()
+
     def test_count_active_bits(self):
         test_value = b'@ABC'
-        self.assertEqual([0, 1, 6, 9, 14, 16, 22, 30], self.active_bits.active_bits(test_value))
+        self.assertEqual(
+            [0, 1, 6, 9, 14, 16, 22, 30],
+            self.test_object.active_bits(test_value)
+        )
