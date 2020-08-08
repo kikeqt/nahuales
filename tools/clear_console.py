bd20e3c130c90699c3b20c46f7a0d10960c40626
Enrique Quijano <kike.qt@gmail.com>
2020-08-08 02:22:59 -0500

    Create clear_console.py

---
+++ tools/clear_console.py
@@ -0,0 +1,15 @@
+__version__ = "$Version: 0.2.0"
+
+from os import system
+import platform
+
+def clear_console():
+	if 'Windows' == platform.system():
+		system('CLS')
+
+	else:
+		system('clear')
+  
+  
+if __name__ == '__main__':
+	clear_console()
