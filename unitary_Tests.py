dec8bb9ea13df4efc2f6b3d4424999be45f48b19
Enrique Quijano <kike.qt@gmail.com>
2020-08-09 13:40:35 -0500

    Create unitary_Tests.py

---
+++ unitary_Tests.py
@@ -0,0 +1,29 @@
+import unittest
+
+# # from tools.active_bits_iterable import Active_bits_iterable_Test
+# # from tools.bytes_2_bit_iterative import Bytes_2_bit_iterative_Test
+# # from tools.bytes_2_decimal_part import Bytes_2_decimal_part_Test
+# # from tools.float_2_bytes import Float_2_bytes_Test
+
+from tools.active_bits import Active_bits_Test
+from tools.binary_string_2_bytes import Binary_string_2_bytes_Test
+from tools.bytes_2_bits import Bytes_2_bits_Test
+from tools.bytes_2_integer import Bytes_2_integer_Test
+from tools.count_active_bits import Count_active_bits_Test
+
+from tools.count_inactive_bits import Count_inactive_bits_Test
+from tools.integer_2_string import Integer_2_string_Test
+from tools.integer_2_bytes import Integer_2_bytes_Test
+from tools.get_byte import Get_byte_Test
+from tools.read_bit import Read_bit_Test
+
+from tools.replace_byte import Replace_byte_Test
+from tools.rotate_on_right_4_bits import Rotate_on_right_4_bits_Test
+from tools.rotate_on_right_4_bytes import Rotate_on_right_4_bytes_Test
+from tools.rotate_on_left_4_bits import Rotate_on_left_4_bits_Test
+from tools.rotate_on_left_4_bytes import Rotate_on_left_4_bytes_Test
+
+from tools.bytes_2_binary_string import Bytes_2_binary_string_Test
+from tools.xor_4_bytes import XOR_4_bytes_Test
+
+unittest.main()
