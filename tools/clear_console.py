65b43cd4350f079fc06ceb951030f40e281080e2
Enrique Quijano <kike.qt@gmail.com>
2020-08-13 11:52:47 -0500

    Update clear_console.py
    Format

--- tools/clear_console.py
+++ tools/clear_console.py
@@ -1,15 +1,16 @@
-__version__ = "$Version: 0.2.0"
+__version__ = "$Version: 0.2.1"
 
 from os import system
 import platform
 
-def clear_console():
-	if 'Windows' == platform.system():
-		system('CLS')
 
-	else:
-		system('clear')
-  
-  
+def clear_console():
+    if 'Windows' == platform.system():
+        system('CLS')
+
+    else:
+        system('clear')
+
+
 if __name__ == '__main__':
-	clear_console()
+    clear_console()
