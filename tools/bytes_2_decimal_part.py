de37e52b8000c50203a20c81900db787a45a6174
Enrique Quijano <kike.qt@gmail.com>
2020-08-13 11:43:41 -0500

    Update bytes_2_decimal_part.py
    Format document

--- tools/bytes_2_decimal_part.py
+++ tools/bytes_2_decimal_part.py
@@ -1,12 +1,12 @@
-__version__ = "$Version: 2.0.0"
+__version__ = "$Version: 2.0.1"
 
 from math import ceil
 
 from .active_bits_iterable import Active_bits_iterable
 from .integer_2_bytes import Integer_2_bytes
 
+
 class Bytes_2_decimal_part(Active_bits_iterable, Integer_2_bytes):
-    
     def bytes_2_decimal_part(self, bytes_parameter: bytes):
         """Convert a set of bytes to the decimal part of a number"""
         if isinstance(bytes_parameter, int):
@@ -21,5 +21,6 @@ class Bytes_2_decimal_part(Active_bits_iterable, Integer_2_bytes):
             return sum
 
         else:
-            print('Fatal error: I do not know how to process the type', type(bytes_parameter))
+            print('Fatal error: I do not know how to process the type',
+                  type(bytes_parameter))
             return None
