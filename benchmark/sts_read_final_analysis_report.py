f8194f5143c2c7bfb4e3bbeb96bb31f1e6e0f1b6
Enrique Quijano <kike.qt@gmail.com>
2020-08-17 21:10:58 -0500

    Update sts_read_final_analysis_report.py
    Integrado con nist_sts_reports

--- benchmark/sts_read_final_analysis_report.py
+++ benchmark/sts_read_final_analysis_report.py
@@ -1,21 +1,22 @@
-__version__ = "$Version: 0.0.1"
+__version__ = "$Version: 0.0.2"
 
 from os.path import exists
 
 
 class STS_read_final_analysis_report(object):
-    """STS_read_final_analysis_report
-    
+    """Read the STS final analysis report
+
     Methods
     -------
     read_from_file(file_name: str) -> dict
         Get the results of the final analisys report.
     """
-    def read_from_file(self, file_name: str) -> dict:
+
+    def read_from_file(self, dict_results: dict, file_name: str) -> dict:
         """read_from_file(file_name: str) -> dict
-        
+
         Get the results of the final analisys report.
-        
+
         PARAMETERS
         ----------
         file_name: str
@@ -26,34 +27,38 @@ class STS_read_final_analysis_report(object):
         Dict
             Results dictionary.
         """
-        results_outputs = []
 
         if exists(file_name):
             with open(file_name, 'r') as file_report:
                 for item in range(7):
                     _ = file_report.readline()
-                    
-                output = {}
+
+                test: str = ""
+                counter: int
 
                 while line := file_report.readline():
                     if len(line) == 1:
                         break
 
+                    # Clear eval string
                     line = line[:-1].replace('  ', ' ')
                     line = line.replace('  ', ' ')
                     line = line.replace('  ', ' ')
                     line = line[1:].split(' ')
-                    test = line[-1]
-                    
-                    if test not in output:
-                        output[test] = []
-                        
+
+                    if line[-1] != test:
+                        counter = 0
+                        test = line[-1]
+
+                    # Search for the category to which the evaluation belongs
                     category = 0
 
                     for item in range(10):
                         if line[item] == '1':
                             category = item + 1
-                            
-                    output[test].append(category)
 
-            return output
+                    dict_results[test][counter].append(category)
+
+                    counter += 1
+
+            return dict_results
