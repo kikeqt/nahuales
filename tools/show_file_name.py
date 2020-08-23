6a76b4d5c95b61dd5a291b4ab6d25647499910b1
Enrique Quijano <kike.qt@gmail.com>
2020-08-22 19:44:28 -0500

    Update show_file_name.py
    Fix incorrect variable

--- tools/show_file_name.py
+++ tools/show_file_name.py
@@ -1,12 +1,13 @@
-__version__ = "$Version: 0.0.2"
+__version__ = "$Version: 0.0.3"
 
 from .split_path_file_extension import split_path_file_extension
 
 
 def show_file_name(full_file_name: str, show_path=False, show_extension=False):
-    """Show the filename, and optionaly path and extension"""
+    """Show the file name, and optionally path and extension"""
     path, file_name, extension = split_path_file_extension(full_file_name)
 
-    output = f"{path}{filename}" if show_path else file_name
+    output = f"{path}{file_name}" if show_path else file_name
     output += extension if show_extension else ""
+
     return output
